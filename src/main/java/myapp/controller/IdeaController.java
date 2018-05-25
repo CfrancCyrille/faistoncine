53b2a1e54a4574290d98b1cb3602e7e88c8ad63c
Marion904 <marionfontaine904@gmail.com>
2018-05-25 14:32:44 +0200

    GetMapping idea/edit

--- src/main/java/myapp/controller/IdeaController.java
+++ src/main/java/myapp/controller/IdeaController.java
@@ -1,11 +1,17 @@
 package myapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import myapp.model.Idea;
+import myapp.model.Scenario;
 import myapp.repository.IdeaRepository;
 import myapp.repository.ScenarioRepository;
 
@@ -20,6 +26,27 @@ public class IdeaController {
 	 	model.addAttribute("ideaList", ideaRepository.findAll());
 	 	return "idea/list";
 	 }
+	
+	
+	@PostMapping("/idea/edit")
+	public String edit(
+				@Valid Idea idea,
+				BindingResult result) {
+			if (result.hasErrors()) {
+	            return "scenarii/show";
+	        }
+			else {
+	      // Retreive idea ide in repository
+	      Idea ide = (ideaRepository.findById(idea.getId())).get();
+	      // Update scenario s:
+				ide.setDescription(idea.getDescription());
+				ide.setName(idea.getName());
+	      // Save scenario edited:
+				ideaRepository.save(ide);
+	      // Return to the list of scenarii
+	      return "redirect:/scenarii";
+			}
+	}
 	
 	@GetMapping(path= {"idea/delete/{id}"})
 	public String delete( Model model, @PathVariable Long id)	{
