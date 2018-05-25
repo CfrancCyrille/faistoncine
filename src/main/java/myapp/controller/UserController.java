67a512d03145f6e190d84e43a758ca1490794425
Nina Mansouri <nina.mansouri@soprasteria.com>
2018-05-25 15:30:20 +0200

    Commite pour adri

--- src/main/java/myapp/controller/UserController.java
+++ src/main/java/myapp/controller/UserController.java
@@ -1,5 +1,66 @@
 package myapp.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import myapp.form.UserForm;
+import myapp.model.User;
+import myapp.repository.UserRepository;
+
+@Controller
 public class UserController {
-// Tu dis quand t'as besoin :)
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	
+	@GetMapping("/user/l")
+	public String userList(Model model) {
+		model.addAttribute("userList", userRepository.findAll());
+		model.addAttribute("userForm", new UserForm());
+		return "user/listUser";
+	}
+	
+	
+	@GetMapping("/user/a")
+	public String userAddGet(Model model) {
+//		model.addAttribute("userList", userRepository.findAll());
+		model.addAttribute("userForm", new UserForm());
+		return "user/addUser";
+	}
+	
+	@PostMapping("/user/add")
+	public String userAdd(@Valid User user, BindingResult result) {
+			if (result.hasErrors()) {
+	            return "user/listUser";
+	        }
+			else {
+	      // Create a new user 
+				User u = new User();
+	      // Add to user u : 
+				u.setFirstname(user.getFirstname());
+				u.setLastname(user.getLastname());
+				u.setUsername(user.getUsername());
+				u.setPassword(user.getPassword());
+	      // Save user added:
+				userRepository.save(u);
+		      // Return to the list of user
+		      return "redirect:/user";
+			}
+	}
+	
+	@GetMapping("/user/{id}")
+	public String userShow(Model model, @PathVariable Long id) {
+			model.addAttribute("user", userRepository.findById(id));
+			return "user/editUser";
+	}
+
+
 }
---
+++ src/main/java/myapp/form/UserForm.java
@@ -0,0 +1,51 @@
+package myapp.form;
+
+
+public class UserForm {
+
+	private Long id;
+	private String firstname;
+	private String lastname;
+	private String username;
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
