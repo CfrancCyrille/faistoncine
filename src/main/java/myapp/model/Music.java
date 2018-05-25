576fd846eea41cce9e1637a4f79aacd3201cd3ef
difol <difol@LAPTOP-VVEET1UT>
2018-05-25 11:58:58 +0200

    model music

---
+++ src/main/java/myapp/model/Music.java
@@ -0,0 +1,45 @@
+package myapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Music {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+	private String title;
+	private String author;
+	private String interpreter;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getInterpreter() {
+		return interpreter;
+	}
+	public void setInterpreter(String interpreter) {
+		this.interpreter = interpreter;
+	}
+	
+	
+	
+}
