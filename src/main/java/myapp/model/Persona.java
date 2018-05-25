7da7f24835fa63c9a55467ad433a12656c9822d4
Unknown <shysao@gmail.com>
2018-05-25 11:12:34 +0200

    EntityPersona

--- src/main/java/myapp/model/Persona.java
+++ src/main/java/myapp/model/Persona.java
@@ -1,5 +1,59 @@
 package myapp.model;
 
-public class Persona {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Persona {
+	@Id
+	private Long id;
+	private String lastname;
+	private String firstname;
+	private String personality;
+	private String gender;
+	
+	public Persona() {
+		super();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getPersonality() {
+		return personality;
+	}
+
+	public void setPersonality(String personality) {
+		this.personality = personality;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
 }
