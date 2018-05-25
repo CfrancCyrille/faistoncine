188e9722d7dc3a1207cf0a914bcd1f7d0797a058
Nigelk <kozlown@gmail.com>
2018-05-25 12:50:46 +0200

    Auto stash before merge of "dev" and "AdrienBelmas/dev"

--- src/main/java/myapp/controller/UserController.java
+++ src/main/java/myapp/controller/UserController.java
@@ -1,5 +1,21 @@
 package myapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import myapp.repository.UserRepository;
+
 public class UserController {
-// Tu dis quand t'as besoin :)
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@GetMapping("/users")
+	public String show(Model model, @PathVariable Long id) {
+			model.addAttribute("users", userRepository);
+			return "user/listUser";
+	}
+	
 }
