10e23bc4ba6ffcd8c5dbedda803d3890ecabc096
adrienbelmas <adrienbelmas@172.29.28.117>
2018-05-25 14:49:44 +0200

    Delete in userController

--- src/main/java/myapp/controller/UserController.java
+++ src/main/java/myapp/controller/UserController.java
@@ -5,17 +5,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import myapp.model.User;
 import myapp.repository.UserRepository;
 
 public class UserController {
-	
+
 	@Autowired
 	UserRepository userRepository;
-	
+
 	@GetMapping("/users")
-	public String show(Model model, @PathVariable Long id) {
-			model.addAttribute("users", userRepository.findAll());
-			return "user/listUser";
+	public String show(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		return "user/listUser";
 	}
-	
+
+	@GetMapping("/user/delete/{id}")
+	public String delete(Model model, @PathVariable Long id) {
+		User userToDelete = (userRepository.findById(id)).get();
+		userRepository.delete(userToDelete);
+		return "user/listUser";
+	}
 }
